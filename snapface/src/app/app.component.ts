fa04fda75ab247fb1d075b6b5e38acbf56fd6da2
Mathias Einkhah <meinkhah@gmail.com>
2022-07-07 16:53:07 +0200

    ng

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -6,16 +6,30 @@ import { FaceSnap } from './models/face-snap.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  mySnap!: FaceSnap;
+export class AppComponent implements OnInit{
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit',
-      'https://caricom.org/wp-content/uploads/Floyd-Morris-Remake-1024x879-1.jpg',
-      new Date(),
-      0,
-    );
+
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit',
+        imageUrl: 'https://caricom.org/wp-content/uploads/Floyd-Morris-Remake-1024x879-1.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+
+      {
+        title: 'TSE',
+        description: 'Ecole d\'ingénieur',
+        imageUrl: 'https://www.univ-st-etienne.fr/_richText-file/ametys-internal%253Asites/ujm/ametys-internal%253Acontents/telecom-saint-etienne/_attribute/presentation/_data/telecom_st_etienne-1030x360_288x812.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Saint-Etienne'
+      }
+
+      
+    ];
   }
 }
--- snapface/src/app/models/face-snap.model.ts
+++ snapface/src/app/models/face-snap.model.ts
@@ -1,10 +1,10 @@
+import { FactoryTarget } from "@angular/compiler";
+
 export class FaceSnap{
-
-    constructor(public title: string,
-                public description: string,
-                public imageUrl: string,
-                public createdDate: Date,
-                public snaps: number){
-
-    }
+  title!: string;
+    description!: string;
+    imageUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
 }
